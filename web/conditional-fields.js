63f6b82579bbb6b7ecdf2b8c96c01534a4cdb88c
Graham May <graham.may@soapbox.co.uk>
2016-11-28 17:37:41 +0000

    $field default added as a last ditch attempt fallback before it fails. Repeaters functionality added.

--- web/conditional-fields.js
+++ web/conditional-fields.js
@@ -2,8 +2,9 @@
 
 // SBTODO: Multiple conditions
 // SBTODO: < and > condition functionality
-// SBTODO: Make condition: yaml use the field name instead of the backend html class/id
-// SBTODO: add support for fields other than select2 fields (e.g. radio buttons, checkboxes)
+// SBTODO: Make condition: yaml use the field name instead of the backend html class/id (kind of does this already, but taxonomies and relations get prepended)
+// SBTODO: add support for fields other than select fields (e.g. checkboxes)
+// SBTODO: On hide, set value to null / empty so they don't still submit (Should clear: true | false be a setting?)
 
 (function ($) {
     'use strict';
@@ -15,6 +16,11 @@
     var allFields = null;
     var fieldName = '';
 
+    // Repeaters
+    var allRepeaterFields = null;
+    var repeaterToAdd     = null;
+    var regexPattern;
+
     // Relations
     var allRelations  = null;
     var relationToAdd = null;
@@ -24,8 +30,10 @@
     var taxonomyToAdd = null;
 
     // Conditional Arrays
-    var fieldsToWatch     = [];
-    var conditionalFields = [];
+    var fieldsToWatch         = [];
+    var repeaterFieldsToWatch = [];
+    var conditionalFields     = [];
+    var repeaterFields        = [];
 
     if (ctConfig !== undefined) {
         allFields    = ctConfig['fields'];
@@ -44,6 +52,35 @@
 
                     // Add to Conditional fields
                     conditionalFields[name] = allFields[name];
+                }
+            }
+
+            // Check repeater fields and build array of repeaters to watch
+            if (allFields.hasOwnProperty(name) && allFields[name].hasOwnProperty('type') && allFields[name]['type'] === 'repeater') {
+                allRepeaterFields = allFields[name]['fields'];
+
+                for (var field in allRepeaterFields) {
+                    if (allRepeaterFields.hasOwnProperty(field) && allRepeaterFields[field].hasOwnProperty('condition')) {
+                        if (allRepeaterFields[field]['condition'].hasOwnProperty('field')) {
+                            fieldName = allRepeaterFields[field]['condition']['field'];
+
+                            regexPattern = 'ft_content_cards\\[\\d+\]\\[type_of_content\\]';
+
+                            if (repeaterFieldsToWatch.indexOf(regexPattern) === -1) {
+                                // Fields to watch the value change of
+                                repeaterFieldsToWatch.push(regexPattern);
+                            }
+
+                            repeaterToAdd = allRepeaterFields[field];
+
+                            // Flag this entry as a repeater sub field
+                            repeaterToAdd['repeater'] = name;
+
+                            // Add this field to the repeaterFields array
+                            //repeaterFields[name + ':' + field] = repeaterToAdd;
+                            repeaterFields[field] = repeaterToAdd;
+                        }
+                    }
                 }
             }
         }
@@ -97,13 +134,18 @@
 
         $(document).on('ready', function () {
             // Check for select2 changes
-            $('select').on('change', function () {
-                var id = $(this).attr('id');
+            $(document).on('change', 'select', function () {
+                var id           = $(this).attr('id');
+                var name         = $(this).attr('name');
+                var watchedValue = $(this).val();
+
+                // Check if the changed select is inside a repeater
+                if ($(this).parents('.repeater-field').length) {
+                    checkRepeaterFields(id, watchedValue);
+                }
 
                 // Check the changed field is one of the fieldsToWatch
                 if (fieldsToWatch.indexOf(id) > -1) {
-                    var watchedValue = $(this).val();
-
                     checkConditionalFields(id, watchedValue);
                 }
             });
@@ -112,11 +154,23 @@
             var len = fieldsToWatch.length;
             var $fieldToWatch;
 
+            // Check conditional fields
             for (i = 0; i < len; i += 1) {
+                console.log('# Checking fields that have conditionals on: "#' + fieldsToWatch[i] + '"');
+
                 $fieldToWatch = $('#' + fieldsToWatch[i]);
 
                 checkConditionalFields($fieldToWatch.attr('id'), $fieldToWatch.val());
             }
+
+            // Check conditional fields inside repeaters
+            runRepeaterCheck();
+
+            // On new repeater item, check for conditional fields
+            $('.repeater-add').on('click', function () {
+                runRepeaterCheck();
+            });
+
         });
     }
 
@@ -129,7 +183,7 @@
                 }
 
                 // Find the bolt fieldset container
-                var $field = $('#' + name);
+                var $field;
 
                 if (conditionalFields[name].hasOwnProperty('type')) {
                     switch (conditionalFields[name]['type']) {
@@ -156,6 +210,11 @@
                             break;
                         case 'geolocation':
                             $field = $('[name*="' + name + '[address]"]');
+
+                            break;
+
+                        default:
+                            $field = $('#' + name);
 
                             break;
                     }
@@ -198,16 +257,111 @@
                 }
 
                 if ((showField && conditionMatched) || (!showField && !conditionMatched)) {
-                    console.log('> Show ' + name);
+                    console.log('--> Show ' + name);
                     $fieldContainer.show();
                 } else {
-                    console.log('> Hide ' + name);
+                    console.log('--> Hide ' + name);
                     $fieldContainer.hide();
                 }
             }
         }
 
         checkTabPanes();
+    }
+
+    function runRepeaterCheck () {
+        // Check conditional fields inside repeaters
+        var $repeaterFieldToWatch;
+        var i;
+        var len = repeaterFieldsToWatch.length;
+
+        for (i = 0; i < len; i += 1) {
+            console.log('# Checking repeater fields that have conditionals on: "name=/' + repeaterFieldsToWatch[i] + '/"');
+
+            $repeaterFieldToWatch = $('select, input, textarea').filter(function () {
+                if (typeof $(this).attr('name') === 'undefined') {
+                    return false;
+                }
+
+                return $(this).attr('name').match(new RegExp(repeaterFieldsToWatch[i]))
+            });
+
+            // If repeater fields have been found, loop through them and check the conditional fields within the repeater-group
+            if ($repeaterFieldToWatch.length) {
+                $repeaterFieldToWatch.each(function () {
+                    checkRepeaterFields($(this).attr('id'), $(this).val());
+                })
+            }
+        }
+    }
+
+    function checkRepeaterFields (id, watchedValue) {
+        // Check the watched value against fields with conditions
+        for (var name in repeaterFields) {
+            if (repeaterFields.hasOwnProperty(name)) {
+                var repeater          = repeaterFields[name]['repeater'];
+                var watchedFieldRegex = new RegExp(repeater + '\\[\\d+\\]\\[' + repeaterFields[name]['condition']['field'] + '\\]');
+                var fieldRegex        = new RegExp(repeater + '\\[\\d+\\]\\[' + name + '\\]');
+                var $watched_field    = $('#' + id);
+
+                if (!watchedFieldRegex.test($watched_field.attr('name'))) {
+                    continue;
+                }
+
+                // Find the bolt fieldset container
+                var $field = $watched_field.closest('.repeater-group').find('select, input, textarea').filter(function () {
+                    if (typeof $(this).attr('name') === 'undefined') {
+                        return false;
+                    }
+
+                    return $(this).attr('name').match(fieldRegex);
+                });
+
+                if ($field.length === 0) {
+                    console.error('Conditional field could not be hidden: ' + name + '. Type = {' + repeaterFields[name]['type'] + '}');
+                }
+
+                var $fieldContainer = $field.closest('.repeater-field');
+
+                // Config value whether to show / hide the field on condition matched
+                var showField = (repeaterFields[name]['condition']['show'] === undefined || repeaterFields[name]['condition']['show']);
+
+                // Check if the value matches
+                var conditionMatched = false;
+
+                if (typeof watchedValue === 'object') {
+                    var i   = 0;
+                    var len = watchedValue.length;
+
+                    for (i = 0; i < len; i += 1) {
+                        if (repeaterFields[name]['condition']['value'].indexOf(watchedValue[i]) > -1) {
+                            conditionMatched = true;
+
+                            break;
+                        }
+                    }
+                } else {
+                    if (repeaterFields[name]['condition']['value'].indexOf(watchedValue) > -1) {
+                        conditionMatched = true;
+                    }
+                }
+
+                // If the condition is a != condition, invert the conditionMatched result
+                if (repeaterFields[name]['condition']['operator'] === '!=' && conditionMatched) {
+                    conditionMatched = false;
+                } else if (repeaterFields[name]['condition']['operator'] === '!=') {
+                    conditionMatched = true;
+                }
+
+                if ((showField && conditionMatched) || (!showField && !conditionMatched)) {
+                    console.log('--> Show ' + repeater + ' field: ' + name);
+                    $fieldContainer.show();
+                } else {
+                    console.log('--> Hide ' + repeater + ' field: ' + name);
+                    $fieldContainer.hide();
+                }
+            }
+        }
     }
 
     function checkTabPanes () {
